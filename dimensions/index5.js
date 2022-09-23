16beee1f26502d2e79497ee63b5f05f22595c288
danishsshaikh <111149903+mef22@users.noreply.github.com>
2022-09-24 01:26:24 +0530

    #2309-#2409

--- dimensions/index5.js
+++ dimensions/index5.js
@@ -26,8 +26,8 @@ var hide = function (id) {
   $(id).style.display = "none";
 };
 
-function editMoocName(){
-   document.getElementById("mooc-id").removeAttribute("disabled");
+function editMoocName() {
+  document.getElementById("mooc-id").removeAttribute("disabled");
 }
 
 function validateMooc(event) {
@@ -54,7 +54,7 @@ function criterionScoreCalc(allInputs, resultId) {
     const el = document.getElementById(id);
 
     if (el) {
-      total += parseInt(el.innerText, 10) || 0;
+      total += parseFloat(el.innerText) || 0;
     }
   });
 
@@ -132,7 +132,7 @@ function criteriaAverage() {
     const critEl = document.getElementById(id);
 
     if (critEl) {
-      total += parseInt(critEl.innerText, 10) || 0;
+      total += parseFloat(critEl.innerText) || 0;
     }
   });
 
@@ -170,16 +170,13 @@ function criteriaAverage() {
     } else if (feedbackAvg <= 1.9) {
       document.getElementById("feedbackXt1").innerHTML =
         "Towards adequate: You are on the path to make an adequate attempt to incorporate the learner-centric activities in the design of this dimension. Reflecting back on the individual indicators for different criteria of the dimension will help you in improving the pedagogy design.";
-    }
-     else if (feedbackAvg <= 2.4) {
+    } else if (feedbackAvg <= 2.4) {
       document.getElementById("feedbackXt1").innerHTML =
         "Adequate: You have made an adequate attempt to incorporate several aspects of the learner-centric approach in the design of this dimension. To improve further, you may reflect back on the design of this dimension by reviewing individual indicators to identify the ones which can still be addressed.";
-    } 
-    else if (feedbackAvg <= 2.9) {
+    } else if (feedbackAvg <= 2.9) {
       document.getElementById("feedbackXt1").innerHTML =
         "Towards proficient: You have made a near proficient attempt to incorporate most aspects of the learner-centric approach in the design of this dimension. To improve further, you may still review individual indicators in this dimension to map the ones which have been missed, and reflect on incorporating those in your future attempt.";
-    }
-     else if (feedbackAvg <= 3) {
+    } else if (feedbackAvg <= 3) {
       document.getElementById("feedbackXt1").innerHTML =
         "Proficient: You have made a proficient attempt to incorporate the learner-centric activities in the design of this dimension. You have successfully implemented all the listed criteria in your design.";
     }
